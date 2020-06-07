afeb402843333c04a10d112e81d832e8e6ca46c1
jaylivecare <58861047+jaylivecare@users.noreply.github.com>
2020-06-07 16:36:19 -0400

    set up routes and data for scatter graph

--- routes/api/api.js
+++ routes/api/api.js
@@ -1,7 +1,12 @@
 const gdpData = require('../../graphData/gdpData');
+const scatterPlotData = require('../../graphData/scatterPlotData');
 
 module.exports = function (app) {
   app.route('/api/gdp-data').get((req, res) => {
     res.status(200).json(gdpData.data);
+  });
+
+  app.route('/api/scatter-plot-data').get((req, res) => {
+    res.status(200).json(scatterPlotData.data);
   });
 };
--- routes/main/pages.js
+++ routes/main/pages.js
@@ -2,4 +2,7 @@ module.exports = function (app) {
   app.route('/').get((req, res) => {
     res.render('index');
   });
+  app.route('/scatter-plot').get((req, res) => {
+    res.render('scatter-plot');
+  });
 };
