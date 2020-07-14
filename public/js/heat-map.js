0a292e18f2c96b81fed2046c794e0d057c91d9eb
jaylivecare <58861047+jaylivecare@users.noreply.github.com>
2020-07-13 21:27:43 -0400

    set up initial heat map

--- public/js/heat-map.js
+++ public/js/heat-map.js
@@ -1,7 +1,37 @@
+const chart = document.querySelector('.heat-map-chart');
+const description = document.querySelector('#description');
+
+const containerWidth = 1100;
+const containerHeight = 650;
+
+const margin = { top: 100, right: 25, bottom: 50, left: 150 };
+const graphWidth = containerWidth - margin.left - margin.right;
+const graphHeight = containerHeight - margin.top - margin.bottom;
+
+const svg = d3
+  .select('.heat-map-chart')
+  .append('svg')
+  .attr('width', containerWidth)
+  .attr('height', containerHeight);
+
+const graph = svg
+  .append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+//set up scales
+
 window.addEventListener('load', async () => {
   const graphData = await getChartData(
     'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json'
   );
-  console.log(graphData);
-  // updateGraph(graphData);
+
+  const yearsArr = graphData.monthlyVariance.map((item) => item.year);
+  const { minYear, maxYear } = {
+    minYear: Math.min(...yearsArr),
+    maxYear: Math.max(...yearsArr),
+  };
+
+  description.innerHTML = `${minYear} - ${maxYear}: base temperature ${graphData.baseTemperature}&#8451;`;
 });
