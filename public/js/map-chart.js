bf96cd62741c7e3925f6efd47d4ed5cb376cf37f
jaylivecare <58861047+jaylivecare@users.noreply.github.com>
2020-07-29 21:29:17 -0400

    map-graph started and added dev concurrently

--- public/js/map-chart.js
+++ public/js/map-chart.js
@@ -1,11 +1,36 @@
+const chart = document.querySelector('.map-chart-chart');
+
+const containerWidth = 1100;
+const containerHeight = 650;
+
+const margin = { top: 100, right: 50, bottom: 50, left: 50 };
+const graphWidth = containerWidth - margin.left - margin.right;
+const graphHeight = containerHeight - margin.top - margin.bottom;
+
+const svg = d3
+  .select('.map-chart-chart')
+  .append('svg')
+  .attr('width', containerWidth)
+  .attr('height', containerHeight);
+
+const graph = svg
+  .append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+const updateMapGraph = (mapData, graphData) => {
+  console.log(mapData);
+};
+
 window.addEventListener('load', async () => {
   const graphData = await getChartData(
     'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
   );
 
-  // map data https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json
+  const mapGraph = await getChartData(
+    'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+  );
 
-  console.log(graphData);
-
-  // updateGraph(graphData);
+  updateMapGraph(mapGraph, graphData);
 });
