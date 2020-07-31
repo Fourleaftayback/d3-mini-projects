a5b0879e6c18391c2da8c2b5b40d83d303926a0e
jaylivecare <58861047+jaylivecare@users.noreply.github.com>
2020-07-30 21:46:14 -0400

    got initial map rendered

--- public/js/map-chart.js
+++ public/js/map-chart.js
@@ -1,9 +1,9 @@
 const chart = document.querySelector('.map-chart-chart');
 
 const containerWidth = 1100;
-const containerHeight = 650;
+const containerHeight = 700;
 
-const margin = { top: 100, right: 50, bottom: 50, left: 50 };
+const margin = { top: 70, right: 50, bottom: 50, left: 50 };
 const graphWidth = containerWidth - margin.left - margin.right;
 const graphHeight = containerHeight - margin.top - margin.bottom;
 
@@ -17,10 +17,30 @@ const graph = svg
   .append('g')
   .attr('width', graphWidth)
   .attr('height', graphHeight)
-  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+  .attr('transform', `translate(${margin.left * 1.9}, ${margin.top})`);
+
+const path = d3.geoPath();
 
 const updateMapGraph = (mapData, graphData) => {
-  console.log(mapData);
+  graph
+    .attr('class', 'counties')
+    .selectAll('path')
+    .data(topojson.feature(mapData, mapData.objects.counties).features)
+    .enter()
+    .append('path')
+    .attr('class', 'county')
+    .attr('fill', function (d) {
+      // var result = education.filter(function (obj) {
+      //   return obj.fips == d.id;
+      // });
+      // if (result[0]) {
+      //   return color(result[0].bachelorsOrHigher);
+      // }
+      // //could not find a matching fips id in the data
+      // return color(0);
+      'black';
+    })
+    .attr('d', path);
 };
 
 window.addEventListener('load', async () => {
