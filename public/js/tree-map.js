a4580a2583d8c1ee0a951cf9346b964efc3876d5
jaylivecare <58861047+jaylivecare@users.noreply.github.com>
2020-08-08 15:29:10 -0400

    made graph larger

--- public/js/tree-map.js
+++ public/js/tree-map.js
@@ -2,10 +2,10 @@ const chart = document.querySelector('.tree-map-chart');
 const buttons = document.querySelectorAll('.graph-picker-button');
 const allData = [];
 
-const containerWidth = 1100;
-const containerHeight = 650;
+const containerWidth = 1500;
+const containerHeight = 850;
 
-const margin = { top: 75, right: 50, bottom: 25, left: 50 };
+const margin = { top: 75, right: 50, bottom: 25, left: 200 };
 const graphWidth = containerWidth - margin.left - margin.right;
 const graphHeight = containerHeight - margin.top - margin.bottom;
 
@@ -26,6 +26,7 @@ const colors = d3
   .range([
     '#5E4FA2',
     '#3288BD',
+    '#489274',
     '#66C2A5',
     '#ABDDA4',
     '#E6F598',
@@ -33,16 +34,18 @@ const colors = d3
     '#F46D43',
     '#D53E4F',
     '#9E0142',
+    '#5F063A',
   ]);
 
 const legendGroup = svg
   .append('g')
   .attr('class', 'legend')
-  .style('font-size', '0.9rem');
+  .attr('transform', `translate(${25}, ${margin.top + 10})`)
+  .style('font-size', '0.8rem');
 
 const legend = d3
   .legendColor()
-  .shapeWidth(40)
+  .shapeWidth(30)
   .orient('vertical')
   .scale(colors)
   .shapePadding(2);
@@ -51,7 +54,7 @@ const legend = d3
 
 const description = svg
   .append('text')
-  .attr('x', containerWidth / 4)
+  .attr('x', containerWidth / 2)
   .attr('y', 45)
   .attr('id', 'title')
   .attr('text-anchor', 'middle')
@@ -75,14 +78,40 @@ const updateGraph = (data) => {
 
   const texts = graph.selectAll('text').data(root.leaves());
 
-  // update
-  // rects
-  //   .attr('x', (d) => d.x0)
-  //   .attr('y', (d) => d.y0)
-  //   .attr('width', (d) => d.x1 - d.x0)
-  //   .attr('height', (d) => d.y1 - d.y0)
-  //   .style('stroke', 'black')
-  //   .style('fill', (d) => colors(d.data.category));
+  const wordWrap = (text, width) => {
+    text.each(function () {
+      var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        x = text.attr('x'),
+        y = text.attr('y'),
+        dy = 0, //parseFloat(text.attr("dy")),
+        tspan = text
+          .text(null)
+          .append('tspan')
+          .attr('x', x)
+          .attr('y', y)
+          .attr('dy', dy + 'em');
+      while ((word = words.pop())) {
+        line.push(word);
+        tspan.text(line.join(' '));
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(' '));
+          line = [word];
+          tspan = text
+            .append('tspan')
+            .attr('x', x)
+            .attr('y', y)
+            .attr('dy', ++lineNumber * lineHeight + dy + 'em')
+            .text(word);
+        }
+      }
+    });
+  };
 
   rects
     .enter()
@@ -100,16 +129,22 @@ const updateGraph = (data) => {
     .append('text')
     .attr('class', 'rect-text')
     .attr('x', (d) => d.x0 + 5)
-    .attr('y', (d) => d.y0 + 20)
+    .attr('y', (d) => {
+      console.log(d.data.name);
+      return d.y0 + 5;
+    })
     .text((d) => {
-      // console.log(d.data.name);
-      // TODO: break text up here
+      if (d.data.name.length > 22) {
+        return d.data.name.slice(0, 21);
+      }
       return d.data.name;
     })
-    .attr('font-size', '8px')
-    .attr('fill', 'white');
+    .call(wordWrap, 25)
+    .attr('text-anchor', 'start')
+    .attr('font-size', '9.5px')
+    .attr('fill', 'black');
 
-  // legendGroup.call(legend);  //TODO: chunk legend and call
+  legendGroup.call(legend); //TODO: chunk legend and call
   // legendGroup
   //   .selectAll('text')
   //   .attr('class', 'legend-text')
@@ -159,9 +194,11 @@ window.addEventListener('load', async () => {
     'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
   );
   allData.push(movieData);
-  const gameSalesData = await getChartData(
+  let gameSalesData = await getChartData(
     ' https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json'
   );
+
+  gameSalesData.children.shift();
 
   allData.push(gameSalesData);
 
