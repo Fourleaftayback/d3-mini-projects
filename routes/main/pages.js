7343e369a7e9398814e27abc90830ffee4755a40
jaylivecare <58861047+jaylivecare@users.noreply.github.com>
2020-08-09 16:40:47 -0400

    active nav-link

--- routes/main/pages.js
+++ routes/main/pages.js
@@ -1,20 +1,20 @@
 module.exports = function (app) {
   app.route('/').get((req, res) => {
-    res.render('index');
+    res.render('index', { path: '/' });
   });
   app.route('/scatter-plot').get((req, res) => {
-    res.render('scatter-plot');
+    res.render('scatter-plot', { path: '/scatter-plot' });
   });
 
   app.route('/heat-map').get((req, res) => {
-    res.render('heat-map');
+    res.render('heat-map', { path: '/heat-map' });
   });
 
   app.route('/map-chart').get((req, res) => {
-    res.render('map-chart');
+    res.render('map-chart', { path: '/map-chart' });
   });
 
   app.route('/tree-map').get((req, res) => {
-    res.render('tree-map');
+    res.render('tree-map', { path: '/tree-map' });
   });
 };
